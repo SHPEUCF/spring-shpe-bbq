62399dfadf3b718e0c89460037639ac2ae6e6c34
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2021-01-31 17:01:17 -0500

    removed landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Landing, Dashboard } from './pages';
+import { Dashboard } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default () => (
 	<Router>
 		<Switch>
-			<Route path = '/' exact component = { Landing } />
-			<Route path = '/dashboard' exact component = { Dashboard } />
+			<Route path = '/' exact component = { Dashboard } />
 		</Switch>
 	</Router>
 );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,14 +3,47 @@ import { Cards, Search } from '../components';
 import { Admin } from './Admin';
 import Logo from '../assets/shpe/shpeucflogo_bb.svg';
 import '../styles/Dashboard.scss';
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle
+} from '@material-ui/core/';
 
 export const Dashboard = () => {
 	const [activeTab, setActiveTab] = useState('dayOne');
 	const [searchInput, setSearchInput] = useState('');
 	const [filterInput, setFilterInput] = useState({});
+	const [video, setVideo] = useState(true);
 
 	let handleSearch = (e) => setSearchInput(e.target.value);
 	let handleFilter = (tags) => setFilterInput(tags);
+
+	const renderVideo = () => {
+		return (
+			<div>
+				<Dialog
+					open = { video }
+					onClose = { () => setVideo(false) }
+				>
+					<DialogTitle>
+						A Message from Our President
+					</DialogTitle>
+					<DialogContent>
+						<DialogContent>
+							<iframe className = 'video' src = 'https://www.youtube-nocookie.com/embed/_bLta_fWIYM' frameborder = '0' allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+						</DialogContent>
+					</DialogContent>
+					<DialogActions>
+						<Button size = 'small' color = 'primary' onClick = { () => setVideo(false) }>
+							Close
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</div>
+		);
+	};
 
 	const Nav = () => {
 		return (
@@ -39,6 +72,7 @@ export const Dashboard = () => {
 
 	return (
 		<div className = 'outerWrap'>
+			 { renderVideo() }
 			<div className = 'App'>
 				{ Nav() }
 				<div className = 'main'>
