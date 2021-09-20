37dd77b36db6e37b1529c4057bf6049018b36741
smontana30 <smontana@knights.ucf.edu>
2021-09-19 23:21:45 -0400

    final changes "

--- src/data/companyData.js
+++ src/data/companyData.js
@@ -4,7 +4,7 @@ export const companyData = {
 		tagline: 'Come innovate with us',
 		description: 'Now, more than ever, we know how important it is to do work that makes a real impact. Every day around the world, we work with exceptional people and the latest and greatest tech to tackle industries’ most challenging problems. Join our team to harness the power of change and shape the future you want to see with the work you do at Accenture. Here, you’ll match your ingenuity with the latest technology to make incredible things. Let’s create something together.',
 		careersLink: 'http://accenture.com/campus',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/99052860227',
 		major: ['Computer Science', 'Aerospace Engineering', 'Computer Engineering', 'Industrial Engineering', 'Information Technology', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace']
@@ -14,7 +14,7 @@ export const companyData = {
 		tagline: "Don't Just Consult. Innovate. Excel. Grow.",
 		description: 'Capco is a global technology and management consultancy specializing in driving digital transformation across the financial services industry',
 		careersLink: 'https://www.capco.com/Careers',
-		zoomLink: 'Insert Zoom Link Here',
+		zoomLink: 'https://ucf.zoom.us/j/98176138870',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -24,7 +24,7 @@ export const companyData = {
 		tagline: 'To achieve this mission, we created a holistic, cybersecurity & privacy platform with a concierge experience.',
 		description: 'BlackCloak provides Concierge Cybersecurity & Privacy™ services to executives, board members, and key personnel in their personal lives to mitigate hacking, financial, and reputational risks to them and their company. Learn more at blackcloak.io',
 		careersLink: 'http://www.blackcloak.io/',
-		zoomLink: 'Insert Zoom Link Here',
+		zoomLink: 'https://ucf.zoom.us/j/98606363584',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -34,7 +34,7 @@ export const companyData = {
 		tagline: 'Make an impact every day',
 		description: 'Our unmatched combination of expert-led, practitioner-sourced and data-driven research steers clients toward the right decisions on the issues that matter most. We are a trusted advisor and an objective resource for more than 14,000 enterprises in more than 100 countries — across all major functions, in every industry and enterprise size.',
 		careersLink: 'https://jobs.gartner.com/campus',
-		zoomLink: 'https://ucf.zoom.us/j/99416219556?',
+		zoomLink: 'https://ucf.zoom.us/j/94857435651',
 		major: ['Computer Engineering', 'Computer Science', 'Information Technology', 'Other' ],
 		position: ['Internship', 'Full Time'],
 		industry: ['Technology', 'Finance', 'Consulting', 'Other']
@@ -44,7 +44,7 @@ export const companyData = {
 		tagline: "Consider if we're a fit for you",
 		description: 'When you work at JPMorgan Chase, you’re not just working at a global financial institution. You’re an integral part of one of the world’s biggest tech companies. In 16 strategic technology locations worldwide, our team of more than 50,000 technologists design, build and deploy everything from enterprise technology initiatives to big data and mobile solutions, as well as innovations in electronic payments, cybersecurity, machine learning and cloud development.',
 		careersLink: 'https://careers.jpmorgan.com/us/en/students/programs?search=&tags=location__Americas__UnitedStatesofAmerica___aoi__Technology__Technology',
-		zoomLink: 'https://ucf.zoom.us/j/96251444600',
+		zoomLink: 'https://ucf.zoom.us/j/92258342846',
 		major: ['Computer Science'],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -54,7 +54,7 @@ export const companyData = {
 		tagline: 'We provide an environment for our people to flourish',
 		description: 'At Kimley-Horn, we do things differently. People—clients and employees—are at the forefront of our business. Clients know we are laser-focused on their success. Employees know our culture and approach to business are built on a desire to see our staff flourish, one and all. Both groups know that with Kimley-Horn, they can expect more and experience better.',
 		careersLink: 'https://www.kimley-horn.com/careers/',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/92639330251',
 		major: ['Civil Engineering', 'Environmental Engineering', 'Mechanical Engineering', 'Information Technology', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Consulting', 'Transportation', 'Construction']
@@ -64,7 +64,7 @@ export const companyData = {
 		tagline: 'Your Mission is Ours.',
 		description: 'Lockheed Martin is a global security and aerospace company that employs approximately 114,000 people worldwide and is principally engaged in the research, design, development, manufacture, integration and sustainment of advanced technology systems, products and services. The Lockheed Martin Space business area includes development of commercial and government satellites, strategic missiles, mission solutions and the deep space exploration lines of business.',
 		careersLink: 'http://www.lockheedmartinjobs.com',
-		zoomLink: 'https://ucf.zoom.us/j/95254729083',
+		zoomLink: 'https://ucf.zoom.us/j/93849452028',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering', 'Industrial Engineering', 'Mechanical Engineering', 'Information Technology', 'Materials Science', 'Civil Engineering', 'Environmental Engineering', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense', 'Research']
@@ -74,7 +74,7 @@ export const companyData = {
 		tagline: 'The Sandia intern experience — meaningful work and enjoyable play',
 		description: 'Sandia National Laboratories is the nation\'s premier science and engineering lab for national security and technology innovation. Sandia\'s mission is to serve our nation. Sandia offers many challenges and rewarding career opportunities for engineers, scientists, computer specialists, students, etc. throughout our mission areas of defense, nonproliferation, energy, and homeland security.',
 		careersLink: 'hhttps://www.sandia.gov/careers',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/99148233997',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering', 'Electrical Engineering', 'Industrial Engineering', 'Mechanical Engineering', 'Information Technology', 'Materials Science', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Research', 'Technology']
@@ -84,7 +84,7 @@ export const companyData = {
 		tagline: 'Come change the world with us!',
 		description: 'Texas Instruments’ products power the world. As one of the world’s largest global leaders in analog and digital semiconductor design and manufacturing, TI provides the technologies that fuel smartphones, digital cameras, medical equipment, motor controls, ultra-thin computing devices, health and fitness products, and much more. Chances are, many pieces of technology that you touch every day – things you don’t even think about, but can’t imagine living without!',
 		careersLink: 'http://careers.ti.com',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/98658542253',
 		major: ['Computer Engineering', 'Computer Science', 'Electrical Engineering', 'Industrial Engineering', 'Mechanical Engineering', 'Information Technology', 'Materials Science'],
 		position: ['Full Time', 'Internship'],
 		industry: ['Technology', 'Research']
@@ -94,7 +94,7 @@ export const companyData = {
 		tagline: 'Create a Lasting Impact',
 		description: 'The Walt Disney Company, together with its subsidiaries and affiliates, is a leading diversified international family entertainment and media enterprise with the following business segments: media networks, parks and resorts, studio entertainment, consumer products and interactive media. From humble beginnings as a cartoon studio in the 1920s to its preeminent name in the entertainment industry today, Disney proudly continues its legacy of creating world-class stories and experiences for every member of the family. Disney’s stories, characters and experiences reach consumers and guests from every corner of the globe. With operations in more than 40 countries, our employees and cast members work together to create entertainment experiences that are both universally and locally cherished. This position is with Walt Disney Parks and Resorts U.S., Inc., which is part of a business segment we call Parks, Experiences and Products. Walt Disney Parks and Resorts U.S., Inc. is an equal opportunity employer. Applicants will receive consideration for employment without regard to race, color, religion, sex, national origin, sexual orientation, gender identity, disability, or protected veteran status or any other basis prohibited by federal, state or local law. Disney fosters a business culture where ideas and decisions from all people help us grow, innovate, create the best stories and be relevant in a rapidly changing world.',
 		careersLink: 'https://jobs.disneycareers.com/',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/95959982736',
 		major: ['Computer Engineering', 'Computer Science', 'Electrical Engineering', 'Industrial Engineering', 'Mechanical Engineering', 'Information Technology', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Entertainment', 'Technology', 'Research', 'Other']
@@ -104,7 +104,7 @@ export const companyData = {
 		tagline: 'Bringing the world closer together',
 		description: 'Facebook is seeking Software Engineer Interns  to join our engineering team. As a Software Engineer at Facebook, you’ll drive the development of the systems behind Facebook\'s products, create web applications that reach billions of people, build high volume servers and be a part of a team that\’s working to help people connect with each other around the globe.',
 		careersLink: 'https://www.facebook.com/careers/v2/jobs/654496918442526/',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/98899477868',
 		major: ['Computer Science', 'Computer Engineering', 'Information Technology'],
 		position: ['Internship'],
 		industry: ['Technology']
@@ -114,7 +114,7 @@ export const companyData = {
 		tagline: 'Life at Intel',
 		description: 'For interns or college graduates, that means having access to amazing career-building opportunities with a global impact.',
 		careersLink: 'https://www.applytracking.com/optin.aspx?c=39FaE%2bMO99xwUHhkN61PBhxeIcSJQ457',
-		zoomLink: 'Insert Zoom Link Here',
+		zoomLink: 'https://ucf.zoom.us/j/92209609772',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -124,7 +124,7 @@ export const companyData = {
 		tagline: 'OUR CULTURE',
 		description: 'With its roots dating back to 1884, Kiewit is an employee-owned organization that operates through a network of subsidiaries in the United States, Canada, and Mexico. Kiewit offers construction and engineering services in a variety of markets including transportation; oil, gas and chemical; power; building; water/wastewater; industrial; and mining. Kiewit had 2019 revenues of $10.3 billion and employs 23,000 staff and craft employees. ',
 		careersLink: 'http://kiewit.com',
-		zoomLink: 'Insert Zoom Link Here',
+		zoomLink: 'https://ucf.zoom.us/j/94837081039',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -134,7 +134,7 @@ export const companyData = {
 		tagline: 'To discover and expand knowledge for the benefit of humanity.',
 		description: 'Lead an innovative and sustainable program of exploration with commercial and international partners to enable human expansion across the solar system and bring new knowledge and opportunities back to Earth. Support growth of the nation\'s economy in space and aeronautics, increase understanding of the universe and our place in it, work with industry to improve America\'s aerospace technologies and advance American leadership.',
 		careersLink: 'https://intern.nasa.gov/',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/96102372079',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -144,7 +144,7 @@ export const companyData = {
 		tagline: 'Explore careers at Leonardo DRS',
 		description: 'Leonardo DRS is a prime contractor, leading technology innovator and supplier of integrated products, services and support to military forces, intelligence agencies and defense contractors worldwide. The company specializes in naval and maritime systems, ground combat mission command and network computing, global satellite communications and network infrastructure, avionics systems, and intelligence and security solutions. Additionally, the company builds power systems and electro-optical/infrared systems for a wide range of commercial customers. Headquartered in Arlington, Virginia, Leonardo DRS is a wholly owned subsidiary of Leonardo S.p.A., which employs more than 47,000 people worldwide. See our full range of capabilities at www.leonardodrs.com.',
 		careersLink: 'https://www.leonardodrs.com/careers/',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/93104847794',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -154,7 +154,7 @@ export const companyData = {
 		tagline: 'By investing in infrastructure & innovative clean energy, we\'re helping create a sustainable energy future for America that is affordable, efficient & clean. ',
 		description: 'NextEra Energy, Inc. (NYSE: NEE) is a leading clean energy company headquartered in Juno Beach, FL. NEE owns Florida Power & Light Company, the largest rate-regulated electric utility in the U.S. as measured by retail electricity produced & sold, & serves more than 5.6M customer accounts, supporting more than 11M residents across Florida with clean, reliable & affordable electricity. NEE also owns a competitive clean energy business, NextEra Energy Resources, LLC, which, together with its affiliated entities, is the world\'s largest generator of renewable energy from the wind & sun & a world leader in battery storage.',
 		careersLink: 'https://www.nexteraenergy.com/company.html',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/98282877318',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -164,7 +164,7 @@ export const companyData = {
 		tagline: 'Explore careers at Caterpillar',
 		description: 'With 2020 sales and revenues of $41.7 billion, Caterpillar Inc. is the world’s leading manufacturer of construction and mining equipment, diesel and natural gas engines, industrial gas turbines, and diesel-electric locomotives. Our longstanding commitment to sustainability inspires us to improve the quality of the environment and the communities where we live and work and provide products and services that help our customers fulfill society’s basic needs—such as shelter, clean water, sanitation, food and reliable power—in a sustainable way.',
 		careersLink: 'https://www.caterpillar.com/en/careers.html',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/96296320251',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -174,7 +174,7 @@ export const companyData = {
 		tagline: 'Larson Design Group is an award-winning, employee-owned Architecture, Engineering, and Consulting firm.',
 		description: 'As an integrated architecture, engineering, and professional consulting services firm, we create solutions with unrivaled depth and breadth of capabilities. Driven by our passion to serve our clients down to every last detail, our multidiscipline team serves as a trusted partner and extension of your organization. Our excellence results time after time bringing projects to life across the country and across diverse industries.',
 		careersLink: 'https://www.facebook.com/careers/students-and-grads/?p[teams][0]=Internship%20-%20Engineering%2C%20Tech%20%26%20Design&p[teams][1]=Internship%20-%20Business&p[teams][2]=Internship%20-%20PhD&p[teams][3]=University%20Grad%20-%20PhD%20%26%20Postdoc&p[teams][4]=University%20Grad%20-%20Engineering%2C%20Tech%20%26%20Design&p[teams][5]=University%20Grad%20-%20Business&teams[0]=Internship%20-%20Engineering%2C%20Tech%20%26%20Design&teams[1]=Internship%20-%20Business&teams[2]=Internship%20-%20PhD&teams[3]=University%20Grad%20-%20PhD%20%26%20Postdoc&teams[4]=University%20Grad%20-%20Engineering%2C%20Tech%20%26%20Design&teams[5]=University%20Grad%20-%20Business',
-		zoomLink: '',
+		zoomLink: 'https://ucf.zoom.us/j/91852106173',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -184,7 +184,7 @@ export const companyData = {
 		tagline: 'People working together as a global enterprise for aerospace industry leadership.',
 		description: 'Boeing is the world\'s largest aerospace company and leading manufacturer of commercial jetliners, defense, space and security systems, and service provider of aftermarket support. As America’s biggest manufacturing exporter, the company supports airlines and U.S. and allied government customers in more than 150 countries. Boeing products and tailored services include commercial and military aircraft, satellites, weapons, electronic and defense systems, launch systems, advanced information and communication systems, and performance-based logistics and training.',
 		careersLink: 'https://www.boeing.com/company/',
-		zoomLink: 'Insert Zoom Link Here',
+		zoomLink: 'https://ucf.zoom.us/j/92039646292',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
