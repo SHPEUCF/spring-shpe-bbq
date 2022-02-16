426d7f9e12ff2f26fb1810d0b89c5602183f3a9c
smontana30 <smontana@knights.ucf.edu>
2022-02-16 01:59:32 -0500

    added zoom links

--- src/data/companyData.js
+++ src/data/companyData.js
@@ -5,7 +5,7 @@ export const companyData = {
 		tagline: 'Our purpose is to deliver on the promise of technology and human ingenuity.',
 		description: 'Now, more than ever, we know how important it is to do work that makes a real impact. Every day around the world, we work with exceptional people and the latest and greatest tech to tackle industries’ most challenging problems. Join our team to harness the power of change and shape the future you want to see with the work you do at Accenture. Here, you’ll match your ingenuity with the latest technology to make incredible things. Let’s create something together.',
 		careersLink: 'https://www.accenture.com/us-en/careers/local/students?c=car_glb_curateddailyconvanityurl_12357107&n=otc_0821',
-		zoomLink: 'https://ucf.zoom.us/j/99052860227',
+		zoomLink: 'https://ucf.zoom.us/j/94013038928',
 		major: ['Computer Science', 'Aerospace Engineering', 'Computer Engineering', 'Industrial Engineering', 'Information Technology', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace']
@@ -15,7 +15,7 @@ export const companyData = {
 		tagline: 'Walt Disney World Resort cast members bring Disney magic to life through creativity, innovation, and technology.',
 		description: 'Since opening in 1971, Walt Disney World Resort has played an important role in many childhood and family memories. Drawing from a rich heritage of storytelling, Walt Disney World Resort cast members bring Disney magic to life through unmatched attention to detail and superior guest service.',
 		careersLink: 'https://jobs.disneycareers.com/',
-		zoomLink: 'https://ucf.zoom.us/j/95959982736',
+		zoomLink: 'https://ucf.zoom.us/j/97509846962',
 		major: ['Computer Engineering', 'Computer Science', 'Electrical Engineering', 'Industrial Engineering', 'Mechanical Engineering', 'Information Technology', 'Other'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Entertainment', 'Technology', 'Research', 'Other']
@@ -25,7 +25,7 @@ export const companyData = {
 		tagline: 'Meta builds technologies that help people connect, find communities, and grow businesses.',
 		description: 'People who choose to build their careers by building with us at Meta help shape a future that will take us beyond what digital connection makes possible today—beyond the constraints of screens, the limits of distance, and even the rules of physics.',
 		careersLink: 'https://www.metacareers.com/students-and-grads/?p[teams][0]=Internship%20-%20Engineering%2C%20Tech%20%26%20Design&p[teams][1]=Internship%20-%20Business&p[teams][2]=Internship%20-%20PhD&p[teams][3]=University%20Grad%20-%20PhD%20%26%20Postdoc&p[teams][4]=University%20Grad%20-%20Engineering%2C%20Tech%20%26%20Design&p[teams][5]=University%20Grad%20-%20Business&teams[0]=Internship%20-%20Engineering%2C%20Tech%20%26%20Design&teams[1]=Internship%20-%20Business&teams[2]=Internship%20-%20PhD&teams[3]=University%20Grad%20-%20PhD%20%26%20Postdoc&teams[4]=University%20Grad%20-%20Engineering%2C%20Tech%20%26%20Design&teams[5]=University%20Grad%20-%20Business',
-		zoomLink: 'https://ucf.zoom.us/j/98899477868',
+		zoomLink: 'https://ucf.zoom.us/j/94815475419',
 		major: ['Computer Science', 'Computer Engineering', 'Information Technology'],
 		position: ['Internship'],
 		industry: ['Technology']
@@ -35,7 +35,7 @@ export const companyData = {
 		tagline: 'NextEra Energy is looking for energy visionaries ready to help us define the next era.',
 		description: 'THE NEXT GREAT MINDS. To power the next era, we need the next great minds. At NextEra Energy, being an intern means more than getting involved in the day-to-day business. You\'ll get exposure to real projects and challenges in an innovative and diverse environment while you gain professional experience. THE INNOVATORS We’re always searching for talented engineers to help us provide America with affordable, reliable and clean electricity in a wide range of fields, including nuclear, power delivery and power generation.',
 		careersLink: 'https://www.nexteraenergy.com/company.html',
-		zoomLink: 'https://ucf.zoom.us/j/98282877318',
+		zoomLink: 'https://ucf.zoom.us/j/97675413025',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -45,7 +45,7 @@ export const companyData = {
 		tagline: 'YOU COULD… CREATE THE IDEA THAT BECOMES A TECHNOLOGY AND IMPACTS JOBS ACROSS THE GLOBE.',
 		description: 'With 2020 sales and revenues of $41.7 billion, Caterpillar Inc. is the world’s leading manufacturer of construction and mining equipment, diesel and natural gas engines, industrial gas turbines, and diesel-electric locomotives. Our longstanding commitment to sustainability inspires us to improve the quality of the environment and the communities where we live and work and provide products and services that help our customers fulfill society’s basic needs—such as shelter, clean water, sanitation, food and reliable power—in a sustainable way.',
 		careersLink: 'https://www.caterpillar.com/en/careers.html',
-		zoomLink: 'https://ucf.zoom.us/j/96296320251',
+		zoomLink: 'https://ucf.zoom.us/j/99060580812',
 		major: ['Aerospace Engineering', 'Computer Engineering', 'Computer Science', 'Electrical Engineering'],
 		position: ['Internship', 'Full Time'],
 		industry: ['Aerospace', 'Technology', 'Defense']
@@ -55,7 +55,7 @@ export const companyData = {
 		tagline: 'Boeing is the world\'s largest aerospace company and leading manufacturer of commercial jetliners, defense, space and security systems, and service provider of aftermarket support.',
 		description: 'Boeing is the world\'s largest aerospace company and leading manufacturer of commercial jetliners, defense, space and security systems, and service provider of aftermarket support. As America’s biggest manufacturing exporter, the company supports airlines and U.S. and allied government customers in more than 150 countries. Boeing products and tailored services include commercial and military aircraft, satellites, weapons, electronic and defense systems, launch systems, advanced information and communication systems, and performance-based logistics and training.',
 		careersLink: 'https://www.boeing.com/company/general-info/',
-		zoomLink: 'https://ucf.zoom.us/j/92039646292',
+		zoomLink: 'https://ucf.zoom.us/j/96949767769',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -65,7 +65,7 @@ export const companyData = {
 		tagline: 'ContinuumCloud offers a spectrum of solutions designed to meet the unique needs of the behavioral health and human services industry.',
 		description: 'ContinuumCloud offers a spectrum of cloud-based software solutions intentionally designed to meet the unique needs of the behavioral health and human services industry. These solutions include an EHR platform, powered by Welligent, and an HCM system, powered by DATIS HR Cloud. Through these offerings, ContinuumCloud empowers organizations to provide high-quality care and deliver on their mission.',
 		careersLink: 'https://apply.workable.com/continuumcloud/',
-		zoomLink: 'https://ucf.zoom.us/j/92039646292',
+		zoomLink: 'https://ucf.zoom.us/j/95265632444',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -75,7 +75,7 @@ export const companyData = {
 		tagline: 'Goldman Sachs Engineers don\'t just make things happen - we make things possible. Learn more at gs.com/careers',
 		description: 'Goldman Sachs Engineers change the world by connecting people and capital with ideas.Join our engineering teams that build massively scalable software and systems, architect low latency solutions, proactively guard against cyber threats, and leverage machine learning alongside financial engineering to continuously turn data into action. Create new businesses, transform finance, and explore a world of opportunity at the speed of markets. Want to push the limit of digital possibilities? Start by going to gs.com/careers',
 		careersLink: 'http://www.goldmansachs.com/careers',
-		zoomLink: 'https://ucf.zoom.us/j/92039646292',
+		zoomLink: 'https://ucf.zoom.us/j/97509846962',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -85,7 +85,7 @@ export const companyData = {
 		tagline: 'Microsoft - Our mission is to empower every person and every organization on the planet to achieve more.',
 		description: 'Microsoft is the worldwide leader in software, services and solutions that help people and businesses realize their full potential. No matter what your passion is, you\'ll find it here. You can work on a multi-billion dollar global business or revolutionary technology that hasn\'t yet left the lab. Join us at Microsoft and start your journey.',
 		careersLink: 'https://careers.microsoft.com/students/us/en',
-		zoomLink: 'https://ucf.zoom.us/j/92039646292',
+		zoomLink: 'https://ucf.zoom.us/j/97700325081',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
@@ -95,7 +95,7 @@ export const companyData = {
 		tagline: 'Figma is a design platform for teams who build products together.',
 		description: 'Figma is a design platform for teams who build products together. Born on the Web, Figma helps teams create, share, test, and ship better designs from start to finish. Whether it’s consolidating tools, simplifying workflows, or collaborating across teams and time zones, Figma makes the design process faster, more efficient, and fun while keeping everyone on the same page.',
 		careersLink: 'https://www.figma.com/file/qS9bhtMj76gUrXQnvQUz15/University-at-Figma?node-id=108%3A2',
-		zoomLink: 'https://ucf.zoom.us/j/92039646292',
+		zoomLink: 'https://ucf.zoom.us/j/97509846962',
 		major: [],
 		position: ['Internship', 'Full Time'],
 		industry: []
